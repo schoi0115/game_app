84c01116205d5924341a68353b7fc48f09359dda
Troy <troyakins99@yahoo.com>
2021-08-25 03:31:38 -0500

    equipment component created

---
+++ src/components/Equipment.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Equipment = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Equipment
+
+//reqs:
+//create front end route to house back end data route (also create backend route)
+//cards- CRD
+//search
+
+// stretch goals: 
+//filter buttons/component 
